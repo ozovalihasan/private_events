443a15bf2e96204ffd4f7a16a7350d705627bc2d
imhilla <hillaryodhiambo282@gmail.com>
2020-07-17 12:48:10 +0300

    update views, add create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
-      
+    @user = User.new(user_params)
+    if @user.save
+      flash[:info] = ['The user was saved succesfully.']
+      redirect_to new_user_path
+    else
+      flash[:info] = @user.errors.full_messages
+      render :new
+    end
   end
   
   def show
       
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
