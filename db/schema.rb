dab2c3b2d19a817f33c484f57d4058cd1e16e660
imhilla <hillaryodhiambo282@gmail.com>
2020-07-17 18:54:25 +0300

    add foreign user_id and migrate add user to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_145942) do
+ActiveRecord::Schema.define(version: 2020_07_17_154734) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
