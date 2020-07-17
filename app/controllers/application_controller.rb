38e887d0841083fc560074e3c8fc23230d8854d3
ozovalihasan <ozovalihasan@gmail.com>
2020-07-17 13:59:55 +0300

    Add current_user helper to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-  def current_user
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
+  helper_method :current_user
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
   
   def show
-      
+    
   end
 
   private
