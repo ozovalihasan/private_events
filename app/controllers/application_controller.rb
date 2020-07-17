6233c896e040557b640b3c1e218a1d85047e7a40
imhilla <hillaryodhiambo282@gmail.com>
2020-07-17 13:28:38 +0300

    add flash for user not found when sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_user
+
+  end
 end
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,11 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       redirect_to user
+    else
+      flash.now[:danger] = 'User not found. Try again.'
+      render 'new'
     end
   end
 end
+
+
