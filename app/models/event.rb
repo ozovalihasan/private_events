1dbf188fd2e4fc0551d31fa1eeaea840bd272abd
ozovalihasan <ozovalihasan@gmail.com>
2020-07-21 13:24:26 +0300

    Refactor past and future class methods of Event by changing related scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,7 @@ class Event < ApplicationRecord
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
   belongs_to :creator, class_name: 'User', foreign_key: "user_id"  
-  def self.past
-    Event.all.where("date < (?) ", Date.today )
-  end
-
-  def self.future
-    Event.all.where("date >= (?) ", Date.today )
-  end
+  scope :past, -> { where("date < (?) ", Date.today ) }
+  scope :future, -> { where("date >= (?) ", Date.today ) }
+  
 end
