e6807e572b712550b0e90e8e37ecc8cc3ac9fb11
imhilla <hillaryodhiambo282@gmail.com>
2020-07-22 13:27:28 +0300

    refactor logic from events index view and create helper methods

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+  def past_events
+    if Event.past.count.zero?
+      render 'no_event'
+       else 
+      render 'event'
+     end
+  end 
+  def upcoming_events
+    if Event.future.count.zero?
+      render 'no_upcoming_events'
+    else
+      render 'future_events'
+    end
+  end
 end
