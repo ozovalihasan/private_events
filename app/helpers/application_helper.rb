e4196f00f1b896d21b551c0ec0fd35052acb436e
ozovalihasan <ozovalihasan@gmail.com>
2020-07-22 12:37:41 +0300

    Refactor application view to make view logicless

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def show_sign_links
+        if current_user 
+            render 'layouts/sign_out_link'
+        else
+            render 'layouts/sign_in_link'
+        end 
+    end
+
+    def show_navigation_items
+        if current_user 
+            render 'layouts/menu_links'
+        end
+    end
 end
