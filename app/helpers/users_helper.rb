ecc321aa09e58a774d5a9bfc5c0ef93594aa9a55
ozovalihasan <ozovalihasan@gmail.com>
2020-07-22 13:02:54 +0300

    Refactor show view of User to remove logic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+    def print_created_events
+        unless @created_events.all.size.zero? 
+            render 'created_events'
+         end
+    end
+
+    def print_previous_events
+        if @previous_events.all.size.zero? 
+            render 'no_previous_events'
+        else
+            render 'previous_events'
+        end 
+    end
+
+    def print_upcoming_events
+        if @upcoming_events.all.size.zero? 
+            render 'no_upcoming_events'
+        else
+            render 'upcoming_events'
+        end 
+    end
 end
