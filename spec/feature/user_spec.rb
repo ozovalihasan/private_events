988f65090d72c9db83a2b0b2be73a7dac2cbe61c
ozovalihasan <ozovalihasan@gmail.com>
2020-07-23 13:13:12 +0300

    Add test for user conrollers

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
-require './spec/support/integration_test_helpers.rb'
 
 RSpec.describe 'users_controller', type: :system do
   before :each do
-    User.create(username: 'hillary')
+    @user1 = User.create(username: 'hasan')
+    @user2 = User.create(username: 'hillary')
   end
 
   it 'creates a user and logs a user in' do
     do_signup('hillary')
+    expect(page).to have_content 'Sign in'
+  end
+
+  it '' do
     do_login('hillary')
-    expect(page).to have_content 'Past Events'
+    create_event('Graduation Party')
+    invite(@user2, @user1,'Graduation Party')
+    expect(page).to have_content @user1.username
   end
 
 end
--- spec/support/integration_test_helpers.rb
+++ spec/support/integration_test_helpers.rb
@@ -3,12 +3,27 @@ module IntegrationTestHelpers
     visit root_path
     click_link('Sign up')
     fill_in('Your username:', with: username)
-    click_button('SUBMIT')
+    click_button('sign_up')
   end
 
   def do_login(username)
     visit root_path
     fill_in('Username', with: username)
-    click_button('SIGN IN')
+    click_button('sign_in')
+  end
+
+  def create_event(title)
+    click_link('Create Event')
+    fill_in('Title', with: title)
+    page.find('#event_date').set("2020-10-23")
+    fill_in('Description', with: 'Very good party')
+    click_button('commit')
+  end
+
+  def invite(creator,invited_user,event)
+    visit root_path
+    click_link(creator)
+    click_link(event)
+    select invited_user.username, :from => "user_id"
   end
 end
