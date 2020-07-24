556902b6aae7278a27cfab76d3edee295816e76c
ozovalihasan <ozovalihasan@gmail.com>
2020-07-24 11:41:54 +0300

    Update user_id of Event with creator_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :invitations
   has_many :attended_event, through: :invitations, source: :event, dependent: :destroy
-  has_many :events, inverse_of: 'creator', class_name: 'Event', dependent: :destroy
+  has_many :events, inverse_of: 'creator', class_name: 'Event', dependent: :destroy, foreign_key: :creator_id
 end
--- db/migrate/20200717154734_add_user_to_events.rb
+++ db/migrate/20200717154734_add_user_to_events.rb
@@ -1,5 +1,6 @@
 class AddUserToEvents < ActiveRecord::Migration[5.2]
   def change
-    add_reference :events, :user, foreign_key: true
+    add_reference :events, :creator, references: :users, index: true
+    add_foreign_key :events, :users, column: :creator_id
   end
 end
